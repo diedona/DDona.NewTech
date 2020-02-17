9b9853684f3692c94604a1b397d1a494489eed42
Diego Doná <diedona@gmail.com>
2020-02-17 19:29:52 -0300

    Add project files.

---
+++ DDona.NewTech.WpfApp/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DDona.NewTech.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                var frmLogin = new frmLogin();
+                if (frmLogin.ShowDialog().GetValueOrDefault())
+                {
+                    new MainWindow().ShowDialog();
+                    Shutdown();
+                }
+            }
+            finally
+            {
+                Shutdown();
+            }
+        }
+    }
+}
